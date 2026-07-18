a5bf701a0f0468947fe17d1c3c982a3373b090db
Gabriel Lee Anderson <gla.ander@gmail.com>
2026-07-18 00:56:23 -0500

    progress

--- modules/entity_system/entity_system.cpp
+++ modules/entity_system/entity_system.cpp
@@ -1,11 +1,10 @@
 
 #include "entity_system.h"
+#include "core/error/error_macros.h"
+#include "core/os/memory.h"
+#include "core/typedefs.h"
 #include "core/variant/variant.h"
 #include <cstdint>
-
-// int EntitySystem::add(int a, int b) {
-//     return a + b;
-// }
 
 void EntitySystem::_bind_methods() {
     // bind gdscript visible methods here
@@ -15,13 +14,16 @@ void EntitySystem::_bind_methods() {
 EntitySystem::EntitySystem() {
     // Initialize class state when created
 }
-// EntitySystem::~EntitySystem() {
-//     // De-initialize class state when destroyed
-// }
+EntitySystem::~EntitySystem() {
+    deinitialize();
+}
 
-
-void EntitySystem::ensure_capacity(uint32_t p_size) {
+void EntitySystem::ensure_capacity_for_n_entities(TypeIndex p_type_index, Size p_size) {
+    ERR_FAIL_COND_MSG(p_type_index >= num_types, "type index is greater than the total number of types");
+    Size cap = caps[p_type_index];
     if (p_size > cap) {
+        bool at_least_1_sub_bit_field = at_least_1_sub_bit_field_list[p_type_index];
+        FieldIndex num_fields = num_fields_list[p_type_index];
         switch (grow_mode) {
             case GROW_QUARTER:
                 cap = MAX((uint32_t)2, cap + ((1 + cap) >> 2));
@@ -48,55 +50,157 @@ void EntitySystem::ensure_capacity(uint32_t p_size) {
         if (at_least_1_sub_bit_field) {
             cap = (cap + 7) & ~7;
         }
-        for (TypeIndex i = 0; i < num_types; i++) {
-            void* data = data_ptrs[i];
-            uint64_t t = type_list[i];
+        for (FieldIndex i = 0; i < num_fields; i++) {
+            void* data = field_data_ptrs[i];
+            uint64_t t = field_type_list[i];
             uint32_t s = get_total_size(t);
             uint32_t total = cap * s;
             data = memrealloc(data, total);
             CRASH_COND_MSG(!data, "Out of memory");
-            i += 1;
         }
-    } else if (p_size < len) {
-        WARN_VERBOSE("reserve() called with a cap smaller than the current size. This is likely a mistake.");
     }
 }
 
-void EntitySystem::reset() {
-    clear();
-    for (TypeIndex i = 0; i < num_types; i++) {
-        void* data = data_ptrs[i];
+void EntitySystem::deinitialize() {
+    if (init_status == DEINITIALIZED) { return; }
+    for (TypeIndex t = 0; t < num_types; t += 1) {
+        clear_entity_list(t);
+        caps[t] = 0;
+    }
+    for (FieldIndex f = 0; f < total_num_fields; f++) {
+        void* data = field_data_ptrs[f];
         if (data) {
             memfree(data);
             data = nullptr;
         }
     }
-    cap = 0;
+    memfree(reinterpret_cast<void*>(field_type_list));
+    memfree(reinterpret_cast<void*>(field_data_ptrs));
+    memfree(reinterpret_cast<void*>(field_allowed_id_list_index));
+    memfree(reinterpret_cast<void*>(stride_list));
+    memfree(reinterpret_cast<void*>(next_unused_index_list));
+    memfree(reinterpret_cast<void*>(first_free_list));
+    memfree(reinterpret_cast<void*>(at_least_1_sub_bit_field_list));
+    memfree(reinterpret_cast<void*>(lens));
+    memfree(reinterpret_cast<void*>(caps));
+    memfree(reinterpret_cast<void*>(field_idx_offset_list));
+    memfree(reinterpret_cast<void*>(num_fields_list));
+    memfree(reinterpret_cast<void*>(free_tracking_field_list));
+    memfree(reinterpret_cast<void*>(gen_tracking_field_list));
+    memfree(reinterpret_cast<void*>(field_allowed_id_list));
+    init_status = DEINITIALIZED;
 }
-void EntitySystem::resize(Size p_size) {
-    if (p_size < len) {
-        for (TypeIndex i = 0; i < num_types; i++) {
-            Type type = type_list[i];
-            if (is_variant(type)) {
-                Variant* data = reinterpret_cast<Variant*>(data_ptrs[i]);
-                for (Size ii = p_size; ii < len; ii++) {
-                    data[ii].~Variant();
-                }
-            }
-        }
-        len = p_size;
-    } else if (p_size > len) {
-        ensure_capacity(p_size);
-        for (TypeIndex i = 0; i < num_types; i++) {
-            Type type = type_list[i];
-            if (is_variant(type)) {
-                Variant* data = reinterpret_cast<Variant*>(data_ptrs[i]);
-                for (Size ii = p_size; ii < len; ii++) {
-                    data[ii].~Variant();
-                }
-                memnew_arr_placement(data + len, p_size - len);
-            }
-        }
-        len = p_size;
+
+void EntitySystem::clear_entity_list(TypeIndex p_type) {
+    ERR_FAIL_COND_MSG(p_type >= num_types, "type index out of bounds for total number of defined types");
+    lens[p_type] = 0;
+    first_free_list[p_type] = 0;
+    next_unused_index_list[p_type] = 0;
+}
+
+void EntitySystem::define_system(TypeIndex p_num_types) {
+    ERR_FAIL_COND_MSG(init_status != UNINIT, "`define_system()` must be the FIRST step in the EntitySystem initialization process");
+    init_status = SET_TOTAL_TYPES;
+    num_types = p_num_types;
+    lens = reinterpret_cast<Size*>(memalloc_zeroed(p_num_types * sizeof(Size)));
+    caps = reinterpret_cast<Size*>(memalloc_zeroed(p_num_types * sizeof(Size)));
+    next_unused_index_list = reinterpret_cast<Size*>(memalloc(p_num_types * sizeof(Size)));
+    first_free_list = reinterpret_cast<Size*>(memalloc_zeroed(p_num_types * sizeof(Size)));
+    at_least_1_sub_bit_field_list = reinterpret_cast<bool*>(memalloc_zeroed(p_num_types * sizeof(bool)));
+    field_idx_offset_list = reinterpret_cast<FieldIndex*>(memalloc_zeroed(p_num_types * sizeof(FieldIndex)));
+    num_fields_list = reinterpret_cast<FieldIndex*>(memalloc_zeroed(p_num_types * sizeof(FieldIndex)));
+    free_tracking_field_list = reinterpret_cast<FieldIndex*>(memalloc_zeroed(p_num_types * sizeof(FieldIndex)));
+    gen_tracking_field_list = reinterpret_cast<FieldIndex*>(memalloc_zeroed(p_num_types * sizeof(FieldIndex)));
+    num_allowed_id_blocks_per_idx = ((num_types + 63) & ~63) >> 6;
+    for (TypeIndex t = 0; t < num_types; t += 1) {
+        next_unused_index_list[t] = 1;
     }
+}
+
+void EntitySystem::define_type(TypeIndex p_type, FieldIndex p_num_fields) {
+    ERR_FAIL_COND_MSG(init_status != SET_TOTAL_TYPES, "`define_type()` calls must be the SECOND step in the EntitySystem initialization process (after `set_total_types()`)");
+    ERR_FAIL_COND_MSG(p_type >= num_types, "type index out of bounds for total number of defined types");
+    ERR_FAIL_COND_MSG(num_fields_list[p_type] != 0, "type was already defined");
+    ERR_FAIL_COND_MSG(p_num_fields != 0, "cannot define a type with zero fields");
+    num_fields_list[p_type] = p_num_fields;
+}
+
+void EntitySystem::define_field_internal(TypeIndex p_type_idx, FieldIndex p_field_idx, Type p_type, bool p_has_allowed_ids, PackedInt32Array p_allowed_ids) {
+    if (init_status == SET_TOTAL_TYPES) {
+        FieldIndex f = 0;
+        for (TypeIndex i = 0; i < num_types; i += 1) {
+            ERR_FAIL_COND_MSG(num_fields_list[i] == 0, "you must call `define_type()` for EVERY type, as defined by `set_total_types()`");
+            field_idx_offset_list[i] = f;
+            f += num_fields_list[i];
+        }
+        total_num_fields = f;
+        field_type_list = reinterpret_cast<Type*>(memalloc_zeroed(total_num_fields * sizeof(Type)));
+        field_allowed_id_list_index = reinterpret_cast<Type*>(memalloc_zeroed(total_num_fields * sizeof(Type)));
+        stride_list = reinterpret_cast<Size*>(memalloc_zeroed(total_num_fields * sizeof(Size)));
+        field_data_ptrs = reinterpret_cast<void**>(memalloc_zeroed(total_num_fields * sizeof(void*)));
+        init_status = DEFINING_ALL_FIELDS;
+    }
+    ERR_FAIL_COND_MSG(init_status != DEFINING_ALL_FIELDS, "`define_xxxxx_field()` calls must be the THRID step in the EntitySystem initialization process (after `set_total_types()` and all `define_type()` calls)");
+    ERR_FAIL_COND_MSG(p_type_idx >= num_types, "type index out of bounds for total number of defined types");
+    ERR_FAIL_COND_MSG(p_field_idx >= num_fields_list[p_type_idx], "field index out of bounds for defined number of fields on this type");
+    FieldIndex fidx = field_idx_offset_list[p_type_idx] + p_field_idx;
+    ERR_FAIL_COND_MSG(field_type_list[fidx] != 0, "field was already defined");
+    ERR_FAIL_COND_MSG(p_type == 0, "no type data defined at all (resolves to NONE)");
+    if (is_sub_int(p_type)) {
+        at_least_1_sub_bit_field_list[p_type_idx] = true;
+    }
+    field_type_list[fidx] = p_type;
+    stride_list[fidx] = get_total_size(p_type);
+    if (p_has_allowed_ids) {
+        field_allowed_id_list_index[fidx] = total_num_id_fields;
+        total_num_id_fields += 1;
+        if (total_num_id_fields <= 1) {
+            field_allowed_id_list = reinterpret_cast<uint64_t*>(memalloc(num_allowed_id_blocks_per_idx * total_num_id_fields * sizeof(uint64_t)));
+        } else {
+            field_allowed_id_list = reinterpret_cast<uint64_t*>(memrealloc(reinterpret_cast<void*>(field_allowed_id_list), num_allowed_id_blocks_per_idx * total_num_id_fields * sizeof(uint64_t)));
+        }
+        int64_t num_allowed = p_allowed_ids.size();
+        for (int64_t a = 0; a < num_allowed_id_blocks_per_idx; a += 1) {
+            if (num_allowed == 0) {
+                field_allowed_id_list[((total_num_id_fields - 1) * num_allowed_id_blocks_per_idx) + a] = UINT64_MAX;
+            } else {
+                field_allowed_id_list[((total_num_id_fields - 1) * num_allowed_id_blocks_per_idx) + a] = 0;
+            }
+        }
+        for (int64_t a = 0; a < num_allowed; a += 1) {
+            ERR_FAIL_COND_MSG(a > num_types, "allowed entity id type index is greater than the largest entity type");
+            set_allowed_type_id(fidx, a);
+        }
+    } else {
+        field_allowed_id_list_index[fidx] = 0;
+    }
+}
+void EntitySystem::define_field(TypeIndex p_type_idx, FieldIndex p_field_idx, Type p_elem) {
+    ERR_FAIL_COND_MSG(is_entity_id(p_elem), "the `ENTITY_ID` type must be defined with `add_entity_id_field()` or `add_fixed_length_entity_id_array_field()`");
+    define_field_internal(p_type_idx, p_field_idx, make_type(p_elem, NONE, 1), false, PackedInt32Array());
+}
+void EntitySystem::define_entity_id_field(TypeIndex p_type_idx, FieldIndex p_field_idx, PackedInt32Array p_allowed_ids = PackedInt32Array()) {
+    define_field_internal(p_type_idx, p_field_idx, make_type(ENTITY_ID, NONE, 1), true, p_allowed_ids);
+}
+void EntitySystem::define_struct_field(TypeIndex p_type_idx, FieldIndex p_field_idx, Type p_elem, Type p_struct) {
+    ERR_FAIL_COND_MSG(!is_numeric(p_elem), "only numeric types (INT, FLOAT, U8, I32, I64, etc...) area allowed in `struct` types (VEC_2, VEC_3, COLOR_4, RECT_2, etc...)");
+    define_field_internal(p_type_idx, p_field_idx, make_type(p_elem, p_struct, 1), false, PackedInt32Array());
+}
+void EntitySystem::define_fixed_length_array_field(TypeIndex p_type_idx, FieldIndex p_field_idx, Type p_elem, Type p_fixed) {
+    ERR_FAIL_COND_MSG(is_entity_id(p_elem), "the `ENTITY_ID` type must be defined with `add_entity_id_field()` or `add_fixed_length_entity_id_array_field()`");
+    define_field_internal(p_type_idx, p_field_idx, make_type(p_elem, NONE, MAX((Type)1, p_fixed)), false, PackedInt32Array());
+}
+void EntitySystem::define_fixed_length_entity_id_array_field(TypeIndex p_type_idx, FieldIndex p_field_idx, Type p_fixed, PackedInt32Array p_allowed_ids = PackedInt32Array()) {
+    define_field_internal(p_type_idx, p_field_idx, make_type(ENTITY_ID, NONE, MAX((Type)1, p_fixed)), true, p_allowed_ids);
+}
+void EntitySystem::define_fixed_length_struct_array_field(TypeIndex p_type_idx, FieldIndex p_field_idx, Type p_elem, Type p_struct, Type p_fixed) {
+    ERR_FAIL_COND_MSG(!is_numeric(p_elem), "only numeric types (INT, FLOAT, U8, I32, I64, etc...) area allowed in `struct` types (VEC_2, VEC_3, COLOR_4, RECT_2, etc...)");
+    define_field_internal(p_type_idx, p_field_idx, make_type(p_elem, p_struct, MAX((Type)1, p_fixed)), false, PackedInt32Array());
+}
+void EntitySystem::finalize_entity_system_layout() {
+    ERR_FAIL_COND_MSG(init_status != DEFINING_ALL_FIELDS, "`finalize_entity_system_layout()` must be the FOURTH and final step in the EntitySystem initialization process (after `set_total_types()`, all `define_type()` calls, and all `define_xxxxx_field()` calls)");
+    for (FieldIndex i = 0; i <= total_num_fields; i += 1) {
+        ERR_FAIL_COND_MSG(field_type_list[i] == 0, "one of the declared fields was not defined");
+    }
+    init_status = FINALIZED;
 }
--- modules/entity_system/entity_system.h
+++ modules/entity_system/entity_system.h
@@ -2,8 +2,6 @@
 #pragma once
 
 #include "core/object/ref_counted.h"
-#include "core/os/memory.h"
-#include "core/templates/local_vector.h"
 #include "core/typedefs.h"
 #include "core/variant/dictionary.h"
 #include "core/variant/variant.h"
@@ -12,25 +10,83 @@
 typedef uint32_t Index;
 typedef uint64_t Type;
 typedef uint32_t Size;
+typedef uint16_t FieldIndex;
 typedef uint16_t TypeIndex;
 typedef int64_t Id;
 typedef uint8_t GrowMode;
 
 class EntitySystem : public RefCounted {
     GDCLASS(EntitySystem, RefCounted);
+public:
+    enum ELEM {
+        NONE = 0,
+        BOOL = 1,
+        U1,
+        U2,
+        U4,
+        U8,
+        I8,
+        U16,
+        I16,
+        U32,
+        I32,
+        U64,
+        I64,
+        F16,
+        F32,
+        F64,
+        ENTITY_ID,
+        VARIANT,
+        BYTE = U8,
+        INT = I64,
+        FLOAT = F64,
+    };
+    enum STRUCT {
+        VEC_2 = 1,
+        VEC_3,
+        VEC_4,
+        COLOR_3,
+        COLOR_4,
+        RECT_2,
+        TRANSFORM_2D,
+        PLANE,
+        QUATERNION,
+        AABB,
+        TRANSFORM_3D,
+        PROJECTION,
+        BASIS,
+        RECT_3 = AABB,
+    };
+    enum GROW {
+        GROW_EXACT,
+        GROW_QUARTER,
+        GROW_HALF,
+        GROW_DOUBLE,
+    };
+
+protected:
+    static void _bind_methods();
+
 private:
-    enum TYPE_LAYOUT {
+    enum LAYOUT {
+        ELEM_MAX = VARIANT,
+        NUM_ELEM_TYPES = ELEM_MAX + 1,
+        ELEM_BITS = 31 - std::__countl_zero((int)ELEM_MAX),
+        ELEM_MASK_LO = (1 << ELEM_BITS) - 1,
         ELEM_SHIFT = 0,
-        ELEM_MASK = 0b1111,
-        STRUCT_SHIFT = ELEM_SHIFT + 4,
-        STRUCT_MASK = 0b11111 << STRUCT_SHIFT,
-        FIXED_LEN_SHIFT = STRUCT_SHIFT + 5,
-        FIXED_LEN_MASK = 0b11111 << FIXED_LEN_SHIFT,
-        MISC_SHIFT = FIXED_LEN_SHIFT + 5,
-        MISC_MASK = 0b1111 << MISC_SHIFT,
-        NUM_ELEM_TYPES = 16,
-        NUM_STRUCT_TYPES = 14,
-        NUM_MISC_TYPES = 3,
+        ELEM_MASK_HI = ELEM_MASK_LO << ELEM_SHIFT,
+        STRUCT_MAX = BASIS,
+        NUM_STRUCT_TYPES = STRUCT_MAX + 1,
+        STRUCT_BITS = 31 - std::__countl_zero((int)STRUCT_MAX),
+        STRUCT_MASK_LO = (1 << STRUCT_BITS) - 1,
+        STRUCT_SHIFT = ELEM_BITS,
+        STRUCT_MASK_HI = STRUCT_MASK_LO << STRUCT_SHIFT,
+        FIXED_MAX = 31,
+        FIXED_MAX_BIASED = FIXED_MAX + 1,
+        FIXED_BITS = 31 - std::__countl_zero(FIXED_MAX),
+        FIXED_MASK_LO = (1 << FIXED_BITS) - 1,
+        FIXED_SHIFT = (int)ELEM_BITS + (int)STRUCT_BITS,
+        FIXED_MASK_HI = FIXED_MASK_LO << FIXED_SHIFT,
     };
     constexpr static const Size ELEM_SIZES[NUM_ELEM_TYPES] = {
         0, // NONE
@@ -49,6 +105,8 @@ private:
         2, // F16
         4, // F32
         8, // F64
+        sizeof(Variant), // VARIANT
+        sizeof(Id), // ENTITY_ID
     };
     constexpr static const Size ELEM_SIZE_SHIFTS[NUM_ELEM_TYPES] = {
         0, // NONE
@@ -67,8 +125,10 @@ private:
         0, // F16
         0, // F32
         0, // F64
+        0, // VARIANT
+        0, // ENTITY_ID
     };
-    constexpr static const Size STRUCT_SIZES[NUM_STRUCT_TYPES] = {
+    constexpr static const Size STRUCT_ELEM_COUNTS[NUM_STRUCT_TYPES] = {
         1, // NONE
         2, // VEC_2
         3, // VEC_3
@@ -84,21 +144,16 @@ private:
         16, // PROJECTION
         9, // BASIS
     };
-    constexpr static const Size MISC_SIZES[NUM_MISC_TYPES] = {
-        0, // NONE
-        sizeof(Variant), // VARIANT
-        8, // ENTITY_ID
-    };
     static const Size VARIANT_SIZE = sizeof(Variant);
     static const Size ENTITY_ID_SIZE = 8;
     _FORCE_INLINE_ static Type get_struct_type(Type p_type) {
-        return (p_type & STRUCT_MASK) >> STRUCT_SHIFT;
+        return (p_type & STRUCT_MASK_HI) >> STRUCT_SHIFT;
     }
     _FORCE_INLINE_ static Size get_struct_size(Type p_type) {
-        return STRUCT_SIZES[get_struct_type(p_type)];
+        return STRUCT_ELEM_COUNTS[get_struct_type(p_type)];
     }
     _FORCE_INLINE_ static Type get_elem_type(Type p_type) {
-        return (p_type & ELEM_MASK) >> ELEM_SHIFT;
+        return (p_type & ELEM_MASK_HI) >> ELEM_SHIFT;
     }
     _FORCE_INLINE_ static Size get_elem_size(Type p_type) {
         return ELEM_SIZES[get_elem_type(p_type)];
@@ -106,123 +161,131 @@ private:
     _FORCE_INLINE_ static Size get_size_shift(Type p_type) {
         return ELEM_SIZE_SHIFTS[get_elem_type(p_type)];
     }
-    _FORCE_INLINE_ static Type get_misc_type(Type p_type) {
-        return (p_type & MISC_MASK) >> MISC_SHIFT;
-    }
-    _FORCE_INLINE_ static Size get_misc_size(Type p_type) {
-        return MISC_SIZES[get_misc_type(p_type)];
-    }
     _FORCE_INLINE_ static Type get_fixed_size(Type p_type) {
-        return MAX((Size)1, (p_type & FIXED_LEN_MASK) >> FIXED_LEN_SHIFT);
+        return ((p_type & FIXED_MASK_HI) >> FIXED_SHIFT) + 1;
     }
     _FORCE_INLINE_ static Size get_total_size(Type p_type) {
-        return ((get_elem_size(p_type) * get_struct_size(p_type) * get_fixed_size(p_type)) >> get_size_shift(p_type)) + get_misc_size(p_type);
+        return ((get_elem_size(p_type) * get_struct_size(p_type) * get_fixed_size(p_type)) >> get_size_shift(p_type));
+    }
+    _FORCE_INLINE_ static bool is_bool(Type p_type) {
+        return ((p_type & ELEM_MASK_HI) >> ELEM_SHIFT) == BOOL;
+    }
+    _FORCE_INLINE_ static bool is_int(Type p_type) {
+        Type elem = ((p_type & ELEM_MASK_HI) >> ELEM_SHIFT);
+        return elem >= U1 && elem <= I64;
+    }
+    _FORCE_INLINE_ static bool is_sub_int(Type p_type) {
+        Type elem = ((p_type & ELEM_MASK_HI) >> ELEM_SHIFT);
+        return elem >= U1 && elem <= U4;
+    }
+    _FORCE_INLINE_ static bool is_whole_int(Type p_type) {
+        Type elem = ((p_type & ELEM_MASK_HI) >> ELEM_SHIFT);
+        return elem >= U8 && elem <= I64;
+    }
+    _FORCE_INLINE_ static bool is_float(Type p_type) {
+        Type elem = ((p_type & ELEM_MASK_HI) >> ELEM_SHIFT);
+        return elem >= F16 && elem <= F64;
+    }
+    _FORCE_INLINE_ static bool is_numeric(Type p_type) {
+        Type elem = ((p_type & ELEM_MASK_HI) >> ELEM_SHIFT);
+        return elem >= U1 && elem <= F64;
+    }
+    _FORCE_INLINE_ static bool is_entity_id(Type p_type) {
+        return ((p_type & ELEM_MASK_HI) >> ELEM_SHIFT) == ENTITY_ID;
     }
     _FORCE_INLINE_ static bool is_variant(Type p_type) {
-        return (p_type & VARIANT) != 0;
+        return ((p_type & ELEM_MASK_HI) >> ELEM_SHIFT) == VARIANT;
     }
-public:
-    enum TYPES {
-        NONE = 0,
-        SCALAR = NONE,
-        // ELEMENTS
-        BOOL = 1 << ELEM_SHIFT,
-        U1 = 2 << ELEM_SHIFT,
-        U2 = 3 << ELEM_SHIFT,
-        U4 = 4 << ELEM_SHIFT,
-        U8 = 5 << ELEM_SHIFT,
-        I8 = 6 << ELEM_SHIFT,
-        U16 = 7 << ELEM_SHIFT,
-        I16 = 8 << ELEM_SHIFT,
-        U32 = 9 << ELEM_SHIFT,
-        I32 = 10 << ELEM_SHIFT,
-        U64 = 11 << ELEM_SHIFT,
-        I64 = 12 << ELEM_SHIFT,
-        F16 = 13 << ELEM_SHIFT,
-        F32 = 14 << ELEM_SHIFT,
-        F64 = 15 << ELEM_SHIFT,
-        INT = I64,
-        FLOAT = F64,
-        // Structs
-        VEC_2 = 1 << STRUCT_SHIFT,
-        VEC_3 = 2 << STRUCT_SHIFT,
-        VEC_4 = 3 << STRUCT_SHIFT,
-        COLOR_3 = 4 << STRUCT_SHIFT,
-        COLOR_4 = 5 << STRUCT_SHIFT,
-        RECT_2 = 6 << STRUCT_SHIFT,
-        TRANSFORM_2D = 7 << STRUCT_SHIFT,
-        PLANE = 8 << STRUCT_SHIFT,
-        QUATERNION = 9 << STRUCT_SHIFT,
-        AABB = 10 << STRUCT_SHIFT,
-        TRANSFORM_3D = 11 << STRUCT_SHIFT,
-        PROJECTION = 12 << STRUCT_SHIFT,
-        BASIS = 13 << STRUCT_SHIFT,
-        RECT_3 = AABB,
-        // Other types
-        VARIANT = 1 << MISC_SHIFT,
-        ENTITY_ID = 2 << MISC_SHIFT,
-    };
-    enum GROW {
-        GROW_EXACT,
-        GROW_QUARTER,
-        GROW_HALF,
-        GROW_DOUBLE,
-    };
-
-    Variant get(Id p_id);
-    void set(Id p_id, Variant p_val);
-    void create(TypeIndex p_type);
-    void set_schema(Dictionary p_schema);
+    _FORCE_INLINE_ static Type make_type(Type p_elem, Type p_struct, Type p_fixed_len) {
+        ERR_FAIL_COND_V_MSG(p_elem >= NUM_ELEM_TYPES, 0, "element type is invalid");
+        ERR_FAIL_COND_V_MSG(p_struct >= NUM_STRUCT_TYPES, 0, "struct type is invalid");
+        ERR_FAIL_COND_V_MSG(p_fixed_len <= 0 || p_fixed_len > FIXED_MAX_BIASED, 0, "fixed len is either zero, or greater than max fixed len");
+        p_fixed_len -= 1;
+        p_elem <<= ELEM_SHIFT;
+        p_struct <<= STRUCT_SHIFT;
+        p_fixed_len <<= FIXED_SHIFT;
+        return p_fixed_len | p_struct | p_elem;
+    }
+    void define_field_internal(TypeIndex p_type_idx, FieldIndex p_field_idx, Type p_field_type, bool p_has_allowed_ids, PackedInt32Array p_allowed_ids);
     
-    // Public vars/methods
-    // int add(int a, int b) // example
+    constexpr static const Id gen_mask_lo = 0xFFFF;
+    constexpr static const Id type_mask_lo = 0xFFFF;
+    constexpr static const Id type_index_shift = 48;
+    constexpr static const Id gen_shift = 32;
+    
+    // these pointer lens = num_types
+    Size* lens = nullptr;
+    Size* caps = nullptr;
+    FieldIndex* field_idx_offset_list = nullptr;
+    FieldIndex* num_fields_list = nullptr;
+    FieldIndex* free_tracking_field_list = nullptr;
+    FieldIndex* gen_tracking_field_list = nullptr;
+    bool* at_least_1_sub_bit_field_list = nullptr;
+    Size* first_free_list = nullptr;
+    Size* next_unused_index_list = nullptr;
+    // these pointer lens = total_num_fields
+    Type* field_type_list = nullptr;
+    void** field_data_ptrs = nullptr;
+    Type* field_allowed_id_list_index = nullptr;
+    Size* stride_list = nullptr;
+    // this pointer len = total_num_id_fields
+    uint64_t* field_allowed_id_list = nullptr;
+    FieldIndex total_num_id_fields = 0;
+    FieldIndex total_num_fields = 0;
+    TypeIndex num_types = 0;
+    TypeIndex next_expected_type_define = 0;
+    FieldIndex num_allowed_id_blocks_per_idx = 0;
+    uint8_t grow_mode = GROW_QUARTER;
+
+    _FORCE_INLINE_ void set_allowed_type_id(FieldIndex p_field_idx, int64_t p_allowed) {
+        int64_t block = p_allowed >> 6;
+        int64_t bit_shift = p_allowed & 63;
+        uint64_t bit = (uint64_t)1 << bit_shift;
+        FieldIndex allowed_block_offset = field_allowed_id_list_index[p_field_idx];
+        field_allowed_id_list[(allowed_block_offset * num_allowed_id_blocks_per_idx) + block] |= bit;
+    }
+    _FORCE_INLINE_ bool type_id_is_allowed_in_field(FieldIndex p_field_idx, TypeIndex p_type_idx) {
+        uint64_t block = (uint64_t)p_type_idx >> 6;
+        uint64_t bit_shift = (uint64_t)p_type_idx & 63;
+        uint64_t bit = (uint64_t)1 << bit_shift;
+        FieldIndex allowed_block_offset = field_allowed_id_list_index[p_field_idx];
+        return (field_allowed_id_list[(allowed_block_offset * num_allowed_id_blocks_per_idx) + block] & bit) == bit;
+    }
+
+    enum INIT {
+        UNINIT,
+        SET_TOTAL_TYPES,
+        ADDING_ALL_TYPES,
+        DEFINING_ALL_FIELDS,
+        FINALIZED,
+        DEINITIALIZED,
+    };
+    uint8_t init_status = UNINIT;
+
+public:
+    void clear_entity_list(TypeIndex p_type);
+	void deinitialize();
+	_FORCE_INLINE_ bool entity_list_is_empty(TypeIndex p_type) const { return lens[p_type] == 0; }
+	_FORCE_INLINE_ bool entity_list_not_empty(TypeIndex p_type) const { return lens[p_type] != 0; }
+	_FORCE_INLINE_ Size get_entity_list_cap(TypeIndex p_type) const { return caps[p_type]; }
+	_FORCE_INLINE_ uint32_t get_entity_list_len(TypeIndex p_type) const { return lens[p_type]; }
+	void ensure_capacity_for_n_entities(TypeIndex p_type_index, Size p_size);
+    Variant get(Id p_id) const;
+    bool set(Id p_id, Variant p_val);
+    void create(FieldIndex p_type);
+    void destroy(Id p_id);
+    // Init process
+    void define_system(TypeIndex p_total_num_types);
+    void define_type(TypeIndex p_type_idx, FieldIndex p_num_fields);
+    void define_field(TypeIndex p_type_idx, FieldIndex p_field_idx, Type p_elem);
+    void define_entity_id_field(TypeIndex p_type_idx, FieldIndex p_field_idx, PackedInt32Array p_allowed_fields);
+    void define_struct_field(TypeIndex p_type_idx, FieldIndex p_field_idx, Type p_elem, Type p_struct);
+    void define_fixed_length_array_field(TypeIndex p_type_idx, FieldIndex p_field_idx, Type p_elem, Type p_fixed);
+    void define_fixed_length_entity_id_array_field(TypeIndex p_type_idx, FieldIndex p_field_idx, Type p_fixed, PackedInt32Array p_allowed_fields);
+    void define_fixed_length_struct_array_field(TypeIndex p_type_idx, FieldIndex p_field_idx, Type p_elem, Type p_struct, Type p_fixed);
+    void finalize_entity_system_layout();
 
     EntitySystem();
     ~EntitySystem();
-
-protected:
-    static void _bind_methods();
-    // Protected vars/methods
-
-private:
-    // Private vars/methods
-    Id type_index_mask = 0;
-    Id type_index_shift = 0;
-    Type* type_list = nullptr;
-    void** data_ptrs = nullptr;
-    Type* allowed_id_list = nullptr;
-    Size len = 0;
-    Size cap = 0;
-    TypeIndex num_types = 0;
-    TypeIndex free_tracking_field = 0;
-    TypeIndex gen_tracking_field = 0;
-    uint8_t grow_mode = GROW_QUARTER;
-    bool is_init = false;
-    bool at_least_1_ent_id_field = false;
-    bool at_least_1_sub_bit_field = false;
-    
-    enum TYPE_RANGE {
-        MIN_BITS_ELEM = BOOL,
-        MAX_BITS_ELEM = U4,
-        MIN_INT_ELEM = U8,
-        MAX_INT_ELEM = I64,
-        MIN_FLOAT_ELEM = F16,
-        MAX_FLOAT_ELEM = F64,
-        MIN_STRUCT = VEC_2,
-        MAX_STRUCT = BASIS,
-        MIN_FIXED_LEN = 0,
-        MAX_FIXED_LEN = 32,
-        MIN_MISC = VARIANT,
-        MAX_MISC = ENTITY_ID,
-    };
-public:
-    void clear();
-	void reset();
-    void resize(uint32_t p_size);
-	_FORCE_INLINE_ bool is_empty() const { return len == 0; }
-	_FORCE_INLINE_ bool not_empty() const { return len != 0; }
-	_FORCE_INLINE_ Size get_cap() const { return cap; }
-	_FORCE_INLINE_ uint32_t get_len() const { return len; }
-	void ensure_capacity(uint32_t p_size);
 };
 
